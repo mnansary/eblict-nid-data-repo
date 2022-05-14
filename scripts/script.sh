f91ca6fd620069cb0905ac13483303c382ffe029
mnansary <silentassasin0111@gmail.com>
2022-05-15 01:56:59 +0600

    data change

--- scripts/script.sh
+++ scripts/script.sh
@@ -2,11 +2,12 @@
 save_path="/home/ansary/WORK/Work/APSIS/datasets/NID/"
 backs_dir="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/styles/"
 
-bn_fonts="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/fonts/bangla/"
-bn_std="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/fonts/bangla/Bangla.ttf"
-en_fonts="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/fonts/english/"
-en_std="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/fonts/english/English.ttf"
-save_dir="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/datasets/"
+backs_dir="/backup2/NID/data/styles/"
+bn_fonts="/backup2/NID/data/fonts/bangla/"
+bn_std="/backup2/NID/data/fonts/bangla/Bangla.ttf"
+en_fonts="/backup2/NID/data/fonts/english/"
+en_std="/backup2/NID/data/fonts/english/English.ttf"
+save_dir="/backup2/NID/data/datasets/"
 
 #-----------------------------------------------------------------------------------------------
 src_path="${save_path}source/"
@@ -17,7 +18,7 @@ card_path="${save_path}cards/"
 #python yolo.py $src_path $card_path $save_path
 
 #-----------------------------------synthetic------------------------------------------
-python lang.py "bangla" "bn" $save_dir $bn_fonts $backs_dir $bn_std --num_samples 100000
-python lang.py "english" "en" $save_dir $en_fonts $backs_dir $en_std --num_samples 40000
+python lang.py "bangla" "bn" $save_dir $bn_fonts $backs_dir $bn_std --num_samples 1000000
+python lang.py "english" "en" $save_dir $en_fonts $backs_dir $en_std --num_samples 100000
 #-----------------------------------synthetic------------------------------------------
 echo succeded
